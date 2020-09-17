36a680f5b371619688329f03d8d1fe59e571bf1b
Tubagus <dutapanggulu@gmail.com>
2020-09-17 22:57:29 +0700

    init commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/bagus2x/room-chat
+
+go 1.13
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/sqs/goreturns v0.0.0-20181028201513-538ac6014518 // indirect
+)
---
+++ server.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	// TypeNewUser -
+	TypeNewUser = iota
+	// TypeNewMessage -
+	TypeNewMessage
+	// TypeUserLeaveChat -
+	TypeUserLeaveChat
+)
+
+// PayloadRequest -
+type PayloadRequest struct {
+	Message string
+}
+
+// PayloadResponse -
+type PayloadResponse struct {
+	Type    int    `json:"type"`
+	Sender  string `json:"sender"`
+	Time    int64  `json:"time"`
+	Message string `json:"message"`
+}
+
+// UserConnection -
+type UserConnection struct {
+	*websocket.Conn
+	Username string
+	Room     string
+}
+
+var connections = make([]*UserConnection, 0)
+
+func handleReadWrite(currConn *UserConnection) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("ERR:", err)
+		}
+	}()
+
+	broadcast(currConn, PayloadResponse{
+		Message: fmt.Sprintf("%s join the chat", currConn.Username),
+		Sender:  currConn.Username,
+		Time:    time.Now().Unix(),
+		Type:    TypeNewUser,
+	})
+
+	for {
+		p := PayloadRequest{}
+		err := currConn.ReadJSON(&p)
+		if err != nil {
+			if err != nil {
+				if strings.Contains(err.Error(), "websocket: close") {
+					broadcast(currConn, PayloadResponse{
+						Message: fmt.Sprintf("%s has left the chat", currConn.Username),
+						Sender:  currConn.Username,
+						Time:    time.Now().Unix(),
+						Type:    TypeUserLeaveChat,
+					})
+					deleteConnection(currConn)
+					return
+				}
+				log.Println("Err:", err.Error())
+				continue
+			}
+		}
+
+		broadcast(currConn, PayloadResponse{
+			Message: p.Message,
+			Sender:  currConn.Username,
+			Time:    time.Now().Unix(),
+			Type:    TypeNewMessage,
+		})
+		log.Println(len(connections))
+	}
+}
+
+func broadcast(currConn *UserConnection, pr PayloadResponse) {
+	for _, conn := range connections {
+		if conn.Room == currConn.Room && conn.Username != currConn.Username {
+			conn.WriteJSON(pr)
+		}
+	}
+}
+
+func deleteConnection(currConn *UserConnection) {
+	filteredConn := make([]*UserConnection, 0)
+	for _, conn := range connections {
+		if currConn != conn {
+			filteredConn = append(filteredConn, conn)
+		}
+	}
+	connections = filteredConn
+}
+
+func main() {
+	http.HandleFunc("/api/ws", func(w http.ResponseWriter, r *http.Request) {
+		room := r.URL.Query().Get("room")
+		username := r.URL.Query().Get("username")
+		if len(room) < 4 || len(username) < 4 {
+			http.Error(w, "Err: Status Unpsrocessable Entity", http.StatusUnprocessableEntity)
+			return
+		}
+		conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+		if err != nil {
+			http.Error(w, "Failed open ws connection", http.StatusBadRequest)
+			return
+		}
+
+		currConn := UserConnection{Conn: conn, Username: username, Room: room}
+		connections = append(connections, &currConn)
+
+		go handleReadWrite(&currConn)
+	})
+	port := os.Getenv("PORT")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
